69719d1ba6b73e8094a1290c7af824ced9052748
Ahmet Ağgöz <79806216+ahmetaggoz@users.noreply.github.com>
2024-03-14 16:42:33 +0300

    dosyaları yukledim ilk angular projem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, HttpClientModule]
 })
 export class AppComponent {
+
+  constructor(){
+  
+  }
   title = 'myapp';
+
+  
 }
